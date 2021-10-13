0364ebb31916f6d92658b9b64d36eeb41ef5f788
Jehu Torres Vega <jehutor@microsoft.com>
2021-10-13 16:37:37 -0700

    .

--- src/main/java/net/mcreator/virtualstore/procedures/MschatCommandExecutedProcedure.java
+++ src/main/java/net/mcreator/virtualstore/procedures/MschatCommandExecutedProcedure.java
@@ -14,39 +14,18 @@ public class MschatCommandExecutedProcedure {
 				VirtualstoreMod.LOGGER.warn("Failed to load dependency entity for procedure MschatCommandExecuted!");
 			return;
 		}
-		if (dependencies.get("cmdparams") == null) {
-			if (!dependencies.containsKey("cmdparams"))
-				VirtualstoreMod.LOGGER.warn("Failed to load dependency cmdparams for procedure MschatCommandExecuted!");
-			return;
-		}
 		Entity entity = (Entity) dependencies.get("entity");
-		HashMap cmdparams = (HashMap) dependencies.get("cmdparams");
-		if ((((new Object() {
-			public String getText() {
-				String param = (String) cmdparams.get("0");
-				if (param != null) {
-					return param;
-				}
-				return "";
-			}
-		}.getText())).equals("Tell"))) {
+		String joinParams = String.join(" ", ((HashMap) dependencies.get("cmdparams")).values());
+		if (joinParams.toLowerCase().contains("tell me about xbox")) {
 			VirtualstoreMod.LOGGER.info("Tell argument sent");
 			{
 				Entity _ent = entity;
 				if (!_ent.world.isRemote && _ent.world.getServer() != null) {
 					_ent.world.getServer().getCommandManager().handleCommand(_ent.getCommandSource().withFeedbackDisabled().withPermissionLevel(4),
-							"/tellraw @a {\"text\":\"The Xbox is the best gaming console!\\nAny other questions?\"}");
+							"/tellraw @a {\"text\":\"The Xbox is the best gaming console!Any other questions?\"}");
 				}
 			}
-		} else if ((((new Object() {
-			public String getText() {
-				String param = (String) cmdparams.get("0");
-				if (param != null) {
-					return param;
-				}
-				return "";
-			}
-		}.getText())).equals("How"))) {
+		} else if (joinParams.toLowerCase().contains("surface")) {
 			{
 				Entity _ent = entity;
 				if (!_ent.world.isRemote && _ent.world.getServer() != null) {
