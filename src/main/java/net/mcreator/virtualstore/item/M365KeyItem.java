afb3b5cb4dad9c13c7fe41985b0fa8441a70ef0a
nick5616 <nbelovoskey@microsoft.com>
2021-10-14 13:59:08 -0800

    Added the missing assets

---
+++ src/main/java/net/mcreator/virtualstore/item/M365KeyItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.virtualstore.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.virtualstore.VirtualstoreModElements;
+
+@VirtualstoreModElements.ModElement.Tag
+public class M365KeyItem extends VirtualstoreModElements.ModElement {
+	@ObjectHolder("virtualstore:m_365_key")
+	public static final Item block = null;
+	public M365KeyItem(VirtualstoreModElements instance) {
+		super(instance, 14);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("m_365_key");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
