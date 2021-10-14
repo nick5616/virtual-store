2b6f96d082c58a6d40650c06cb43a8a2437bd537
patel3989 <patelp@DESKTOP-VG3VDCT.redmond.corp.microsoft.com>
2021-10-14 14:57:47 -0700

    cart implementation

--- src/main/java/net/mcreator/virtualstore/item/M365KeyItem.java
+++ src/main/java/net/mcreator/virtualstore/item/M365KeyItem.java
@@ -1,24 +1,29 @@
 
 package net.mcreator.virtualstore.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.virtualstore.VirtualstoreModElements;
+
 @VirtualstoreModElements.ModElement.Tag
 public class M365KeyItem extends VirtualstoreModElements.ModElement {
-
 	@ObjectHolder("virtualstore:m_365_key")
 	public static final Item block = null;
-
 	public M365KeyItem(VirtualstoreModElements instance) {
 		super(instance, 14);
-
 	}
 
 	@Override
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
 			setRegistryName("m_365_key");
@@ -38,7 +43,5 @@ public class M365KeyItem extends VirtualstoreModElements.ModElement {
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
-
 	}
-
 }
--- src/main/java/net/mcreator/virtualstore/item/SurfacepenItem.java
+++ src/main/java/net/mcreator/virtualstore/item/SurfacepenItem.java
@@ -1,24 +1,35 @@
 
 package net.mcreator.virtualstore.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.virtualstore.VirtualstoreModElements;
+
+import java.util.List;
+
 @VirtualstoreModElements.ModElement.Tag
 public class SurfacepenItem extends VirtualstoreModElements.ModElement {
-
 	@ObjectHolder("virtualstore:surfacepen")
 	public static final Item block = null;
-
 	public SurfacepenItem(VirtualstoreModElements instance) {
-		super(instance, 7);
-
+		super(instance, 16);
 	}
 
 	@Override
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
 			setRegistryName("surfacepen");
@@ -42,9 +53,7 @@ public class SurfacepenItem extends VirtualstoreModElements.ModElement {
 		@Override
 		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
 			super.addInformation(itemstack, world, list, flag);
-			list.add(new StringTextComponent("Microsoft Surface Pen"));
+			list.add(new StringTextComponent("surface pen"));
 		}
-
 	}
-
 }
